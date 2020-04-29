39dc9116e53799d0a76096fd1d09135e3b9d2967
Shawn Dermer <shawn.dermer@gmail.com>
2020-04-28 21:59:09 -0400

    userlogin and overall improvements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,13 @@
-import React, { useState } from 'react';
-import { Container, Paper, TextField, Typography } from "@material-ui/core"
-import io from 'socket.io-client';
+import React from 'react';
+import UsernameForm from "./UsernameForm";
+import ChatContainer from "./ChatContainer";
 import './App.css';
 
-const messageHTMLList = (msgs: string[]): React.ReactElement[] => {
-  return msgs.map((msg: string) => <Typography>{msg}</Typography>);
-}
-
-const socket = io('http://localhost:8080/');
-
-socket.on('connect', () => {
-  console.log(socket.connected); // true
-});
-
-socket.emit('username', "Christina");
-
 function App() {
-  const [messageList, setMessageList] = useState<string[]>([])
-
-  socket.on('chat_message', (data: string) => {
-    setMessageList(messageList.concat(data));
-  });
-
   return (
-    <div className="App">
-      <Container>
-        <Paper style={{maxHeight: "80%", height: "80vh", overflow: "auto"}}>
-          {messageHTMLList(messageList)}
-        </Paper>
-        <TextField
-          id="outlined-textarea-static"
-          label="Write a Message!"
-          multiline
-          rows="1"
-          rowsMax="4"
-          variant="outlined"
-          fullWidth
-          onKeyPress={(ev: any) => {
-            if (ev.key === 'Enter') {
-              ev.preventDefault();
-              socket.emit('chat_message', ev.target.value)
-            }
-          }}
-        />
-      </Container>
+    <div className="ChatChat">
+      <UsernameForm />
+      <ChatContainer />
     </div>
   );
 }
